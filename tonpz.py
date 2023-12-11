8aec3a43121c8348520873e115cd567f5d8709ea
wuhaohao <1611499758@qq.com>
2023-12-11 16:18:39 +0800

    fix: tonpz

--- tonpz.py
+++ tonpz.py
@@ -3,11 +3,11 @@ import csv
 import numpy as np
 
 # 检测编码
-with open('datasets/citeulike-a-master/raw-data.csv', 'rb') as f:
+with open('MetaKG/datasets/citeulike-a-master/raw-data.csv', 'rb') as f:
     result = chardet.detect(f.read())
 
 # 使用检测到的编码加载数据
-with open('datasets/citeulike-a-master/raw-data.csv', 'r', encoding=result['encoding']) as f:
+with open('MetaKG/datasets/citeulike-a-master/raw-data.csv', 'r', encoding=result['encoding']) as f:
     # 使用csv.reader解析CSV文件
     csv_reader = csv.reader(f, delimiter=',')
     
@@ -15,10 +15,10 @@ with open('datasets/citeulike-a-master/raw-data.csv', 'r', encoding=result['enco
     header = next(csv_reader)
     
     # 读取数据
-    data = np.genfromtxt(csv_reader, delimiter=',', dtype=None, names=True, missing_values='', filling_values=np.nan)
+    data = np.genfromtxt(f, delimiter=',', dtype=None, names=True, missing_values='', filling_values=np.nan)
 
 # 打印数据
 print(data)
 
 # 将数据保存到 NPZ 文件
-np.savez('data.npz', data=data)
+np.savez('MetaKG/data.npz', data=data)
