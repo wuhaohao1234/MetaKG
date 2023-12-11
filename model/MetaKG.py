0b4e3669c4c11d82cbc578ebcb9de79ae4b0ebd0
wuhaohao <1611499758@qq.com>
2023-12-11 12:28:02 +0800

    update: docs

--- model/MetaKG.py
+++ model/MetaKG.py
@@ -1,9 +1,7 @@
-import random
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_scatter import scatter_mean, scatter_sum, scatter_max
+from torch_scatter import scatter_sum
 from torch_scatter.utils import broadcast
 from collections import OrderedDict
 
@@ -12,29 +10,23 @@ class Attention(nn.Module):
         super(Attention, self).__init__()
 
     def forward(self, entity_agg, user_emb):
-        # Assuming entity_agg and user_emb are of the same size
-        attention_scores = F.cosine_similarity(entity_agg, user_emb, dim=1)
-        attention_weights = F.softmax(attention_scores, dim=0)
+        attention_scores = F.pairwise_distance(entity_agg, user_emb, p=2)
+        attention_weights = F.softmax(-attention_scores, dim=0)
         attention_output = torch.matmul(attention_weights, entity_agg)
         return attention_output
-
 
 class CFModule(nn.Module):
     def __init__(self, n_users, n_items):
         super(CFModule, self).__init__()
 
-    def forward(self, user_emb, interact_mat, weight):
+    def forward(self, user_emb, interact_mat, weight, n_items):
         mat_row = interact_mat._indices()[0, :]
         mat_col = interact_mat._indices()[1, :]
         item_neigh_emb = user_emb[mat_row] * weight[0]
-        i_u_agg = scatter_mean(src=item_neigh_emb, index=mat_col, dim_size=n_items, dim=0)
-
+        i_u_agg = scatter_sum(src=item_neigh_emb, index=mat_col, dim_size=n_items, dim=0)
         return i_u_agg
 
 class Aggregator(nn.Module):
-    """
-    Relational Path-aware Convolution Network
-    """
     def __init__(self, n_users, n_items, triplet_attention, use_gate, use_cf_module, use_attention):
         super(Aggregator, self).__init__()
         self.n_users = n_users
@@ -46,24 +38,27 @@ class Aggregator(nn.Module):
 
         self.gate1 = nn.Linear(64, 64, bias=False)
         self.gate2 = nn.Linear(64, 64, bias=False)
-        self.sigmoid = nn.Sigmoid()
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+
         if self.use_cf_module:
             self.cf_module = CFModule(n_users, n_items)
 
         if self.use_attention:
             self.attention = Attention()
 
-    def scatter_softmax(self, src, index, dim: int = -1, eps: float = 1e-12):
+    def scatter_softmax(self, src, index, dim: int = -1, eps: float = 1e-12, max_per_element=None):
+        if max_per_element is None:
+            max_per_element = src.max()
         if not torch.is_floating_point(src):
             raise ValueError('`scatter_softmax` can only be computed over tensors '
                              'with floating point data types.')
 
         index = broadcast(index, src, dim)
 
-        max_value_per_index = scatter_max(src, index, dim=dim)[0]
+        max_value_per_index = torch.scatter_add(src, index, dim=dim)[0]
         max_per_src_element = max_value_per_index.gather(dim, index)
 
-        recentered_scores = src - max_per_src_element
+        recentered_scores = src - max_per_element
         recentered_scores_exp = recentered_scores.exp()
 
         sum_per_index = scatter_sum(recentered_scores_exp, index, dim)
@@ -77,17 +72,18 @@ class Aggregator(nn.Module):
         head, tail = edge_index
         edge_relation_emb = weight[edge_type]
         neigh_relation_emb = entity_emb[tail] * edge_relation_emb
-        entity_agg = scatter_mean(src=neigh_relation_emb, index=head, dim_size=n_entities, dim=0)
+        entity_agg = scatter_sum(src=neigh_relation_emb, index=head, dim_size=n_entities, dim=0)
 
         return entity_agg
+
     def CF_forward(self, user_emb, interact_mat, weight):
         mat_row = interact_mat._indices()[0, :]
         mat_col = interact_mat._indices()[1, :]
-        mat_val = interact_mat._values()
-
         item_neigh_emb = user_emb[mat_row] * weight[0]
-        i_u_agg = scatter_mean(src=item_neigh_emb, index=mat_col, dim_size=self.n_items, dim=0)
-    def forward(self, entity_emb, user_emb, edge_index, edge_type, interact_mat, weight, fast_weights=None, i=0):
+        i_u_agg = scatter_sum(src=item_neigh_emb, index=mat_col, dim_size=self.n_items, dim=0)
+        return i_u_agg
+
+    def forward(self, entity_emb, user_emb, edge_index, edge_type, interact_mat, weight, fast_weights=None, i=0, mat_row=None, mat_col=None, mat_val=None):
         """KG aggregate"""
         entity_agg = self.KG_forward(entity_emb, edge_index, edge_type, weight)
 
@@ -96,13 +92,13 @@ class Aggregator(nn.Module):
             i_u_agg = self.CF_forward(user_emb, interact_mat, weight)
 
             if fast_weights is None:
-                gi = self.sigmoid(self.gate1(entity_agg[:self.n_items]) + self.gate2(i_u_agg))
+                gi = self.leaky_relu(self.gate1(entity_agg[:self.n_items]) + self.gate2(i_u_agg))
             else:
                 gate1_name = 'convs.{}.gate1.weight'.format(str(i))
                 gate2_name = 'convs.{}.gate2.weight'.format(str(i))
                 conv_w1 = fast_weights[gate1_name]
                 conv_w2 = fast_weights[gate2_name]
-                gi = self.sigmoid(F.linear(entity_agg[:self.n_items], conv_w1) + F.linear(i_u_agg, conv_w2))
+                gi = self.leaky_relu(F.linear(entity_agg[:self.n_items], conv_w1) + F.linear(i_u_agg, conv_w2))
 
             item_emb_fusion = (gi * entity_agg[:self.n_items]) + ((1 - gi) * i_u_agg)
             user_item_mat = torch.sparse.FloatTensor(torch.cat([mat_row, mat_col]).view(2, -1),
@@ -120,14 +116,9 @@ class Aggregator(nn.Module):
 
         return entity_agg, user_agg
 
-
-        return i_u_agg
 class GraphConv(nn.Module):
-    """
-    Graph Convolutional Network
-    """
     def __init__(self, channel, n_hops, n_users, n_relations, n_items, use_gate,
-                  node_dropout_rate=0.5, mess_dropout_rate=0.1):
+                 node_dropout_rate=0.5, mess_dropout_rate=0.1):
         super(GraphConv, self).__init__()
 
         self.convs = nn.ModuleList()
@@ -136,71 +127,38 @@ class GraphConv(nn.Module):
         self.n_items = n_items
         self.node_dropout_rate = node_dropout_rate
         self.mess_dropout_rate = mess_dropout_rate
-        self.triplet_attention = self.Consis_attention()
 
-        weight = nn.init.xavier_uniform_(torch.empty(n_relations, channel))  # not include interact
-        self.weight = nn.Parameter(weight)  # [n_relations - 1, in_channel]
+        weight = nn.init.xavier_uniform_(torch.empty(n_relations, channel))
+        self.weight = nn.Parameter(weight)
 
         for i in range(n_hops):
             self.convs.append(Aggregator(n_users=n_users, n_items=n_items, triplet_attention=self.triplet_attention, use_gate=use_gate))
 
-        self.dropout = nn.Dropout(p=mess_dropout_rate)  # mess dropout
-
-    def Consis_attention(self):
-        # used in KCAN (CIKM 21), no parameter
-        return nn.CosineSimilarity(dim=1, eps=1e-6)
-
-    def _edge_sampling(self, edge_index, edge_type, rate=0.5):
-        # edge_index: [2, -1]
-        # edge_type: [-1]
-        n_edges = edge_index.shape[1]
-        random_indices = np.random.choice(n_edges, size=int(n_edges * rate), replace=False)
-        return edge_index[:, random_indices], edge_type[random_indices]
-
-    def _sparse_dropout(self, x, rate=0.5):
-        noise_shape = x._nnz()
-
-        random_tensor = rate
-        random_tensor += torch.rand(noise_shape).to(x.device)
-        dropout_mask = torch.floor(random_tensor).type(torch.bool)
-        i = x._indices()
-        v = x._values()
-
-        i = i[:, dropout_mask]
-        v = v[dropout_mask]
-
-        out = torch.sparse.FloatTensor(i, v, x.shape).to(x.device)
-        return out * (1. / (1 - rate))
+        self.dropout = nn.Dropout(p=mess_dropout_rate)
 
     def forward(self, user_emb, entity_emb, edge_index, edge_type,
                 interact_mat, fast_weights=None, mess_dropout=True, node_dropout=True):
-
-        """node dropout"""
         if node_dropout:
             edge_index, edge_type = self._edge_sampling(edge_index, edge_type, self.node_dropout_rate)
-            # interact_mat = self._sparse_dropout(interact_mat, self.node_dropout_rate)
 
-        entity_res_emb = entity_emb  # [n_entity, channel]
-        user_res_emb = user_emb  # [n_users, channel]
+        entity_res_emb = entity_emb
+        user_res_emb = user_emb
 
         for i in range(len(self.convs)):
             entity_emb, user_emb = self.convs[i](entity_emb, user_emb,
                                                  edge_index, edge_type, interact_mat,
                                                  self.weight, fast_weights, i=i)
 
-            """message dropout"""
             if mess_dropout:
                 entity_emb = self.dropout(entity_emb)
                 user_emb = self.dropout(user_emb)
             entity_emb = F.normalize(entity_emb)
             user_emb = F.normalize(user_emb)
 
-            """result emb"""
             entity_res_emb = torch.add(entity_res_emb, entity_emb)
             user_res_emb = torch.add(user_res_emb, user_emb)
 
         return entity_res_emb, user_res_emb
-
 
 class Recommender(nn.Module):
     def __init__(self, data_config, args_config, graph, user_pre_embed, item_pre_embed):
@@ -210,11 +168,10 @@ class Recommender(nn.Module):
         self.n_items = data_config['n_items']
         self.n_relations = data_config['n_relations']
         self.n_entities = data_config['n_entities']
-        self.n_nodes = data_config['n_nodes']  # n_users + n_entities
+        self.n_nodes = data_config['n_nodes']
         self.user_pre_embed = user_pre_embed
         self.item_pre_embed = item_pre_embed
 
-        # inner meta-learning update
         self.num_inner_update = args_config.num_inner_update
         self.meta_update_lr = args_config.meta_update_lr
 
@@ -237,12 +194,11 @@ class Recommender(nn.Module):
     def _init_weight(self):
         self.all_embed = nn.init.xavier_uniform_(torch.empty(self.n_nodes, self.emb_size))
 
-        if self.user_pre_embed!=None and self.item_pre_embed!=None:
-            entity_emb = self.all_embed[(self.n_users + self.n_items):,:]
+        if self.user_pre_embed is not None and self.item_pre_embed is not None:
+            entity_emb = self.all_embed[(self.n_users + self.n_items):, :]
             self.all_embed = torch.cat([self.user_pre_embed, self.item_pre_embed, entity_emb])
 
         self.all_embed = nn.Parameter(self.all_embed)
-
 
     def _init_model(self):
         return GraphConv(channel=self.emb_size,
@@ -255,18 +211,10 @@ class Recommender(nn.Module):
                          mess_dropout_rate=self.mess_dropout_rate)
 
     def _get_edges(self, graph):
-        graph_tensor = torch.tensor(list(graph.edges))  # [-1, 3]
-        index = graph_tensor[:, :-1]  # [-1, 2]
-        type = graph_tensor[:, -1]  # [-1, 1]
+        graph_tensor = torch.tensor(list(graph.edges))
+        index = graph_tensor[:, :-1]
+        type = graph_tensor[:, -1]
         return index.t().long().to(self.device), type.long().to(self.device)
-
-    def get_parameter(self):
-        param_dict = dict()
-        for name, para in self.gcn.named_parameters():
-            if name.startswith('conv'):
-                param_dict[name] = para
-
-        return OrderedDict(param_dict)
 
     def forward_kg(self, h, r, pos_t, neg_t):
         entity_emb = self.all_embed[self.n_users:, :]
@@ -297,7 +245,7 @@ class Recommender(nn.Module):
         user_emb = self.all_embed[:self.n_users, :]
         entity_emb = self.all_embed[self.n_users:, :]
 
-        if fast_weights==None:
+        if fast_weights is None:
             fast_weights = self.get_parameter()
 
         for i in range(self.num_inner_update):
@@ -333,8 +281,10 @@ class Recommender(nn.Module):
 
         return loss
 
-    def forward(self, batch=None, is_apapt=False):
-        if is_apapt:
+    # 这里开始添加新的优化代码
+
+    def forward(self, batch=None, is_adapt=False):
+        if is_adapt:
             user = batch['users']
             pos_item = batch['pos_items']
             neg_item = batch['neg_items']
